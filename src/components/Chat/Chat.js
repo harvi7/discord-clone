feef1fc28eaa0406f081e48ab30918844b17af82
harvi7 <iamharshvirani7@gmail.com>
2021-02-10 20:04:46 -0700

    Chat UI

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard'
+import GifIcon from '@material-ui/icons/Gif'
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions'
 
 import './Chat.css'
 import ChatHeader from './ChatHeader'
@@ -15,8 +18,14 @@ function Chat() {
                 <AddCircleIcon />
                 <form>
                     <input placeholder={`Message #React`} />
-                    <button type="submit">Send Message</button>
+                    <button className="chat__inputButton" type="submit">Send Message</button>
                 </form>
+
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large" />
+                    <GifIcon fontSize="large" />
+                    <EmojiEmotionsIcon fontSize="large" />
+                </div>
             </div>
         </div>
     )
