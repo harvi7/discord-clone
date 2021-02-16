737db8d548a948d8e658804434166e3adcb903fa
harvi7 <iamharshvirani7@gmail.com>
2021-02-15 18:40:29 -0700

    Added Chat functionality

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -3,17 +3,19 @@ import { Avatar } from '@material-ui/core'
 
 import './Message.css' 
 
-function Message() {
+function Message({ timestamp, message, user }) {
     return (
         <div className="message">
-            <Avatar />
+            <Avatar src={user.photo} />
             <div className="message__info">
                 <h4>
-                    CaptainHarvi
-                    <span class="message__timestamp">Timestamp</span>    
+                    {user.displayName}
+                    <span class="message__timestamp">
+                        {new Date(timestamp?.toDate()).toUTCString()}    
+                    </span>    
                 </h4>
 
-                <p>Some message</p>
+                <p>{message}</p>
             </div>
 
         </div>
